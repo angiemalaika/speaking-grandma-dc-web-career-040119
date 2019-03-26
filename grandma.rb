df5cdab93f55f372c8a669dd0597902a57d7faa9
Angela Mumba <malaika.mumba@gmail.com>
2019-03-26 20:26:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,23 +16,18 @@ def speak_to_grandma(speak)
 
     if speak == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-    # If you shout, she can hear you (or at least she thinks so)
-    # and yells back
     
     elsif speak == speak.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-  		else return 
-  		  "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
+  		return  "HUH?! SPEAK UP, SONNY!"
+    elsif speak == speak.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
+      
+      
+    elsif speak == speak.upcase
       return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
     else
   		return "NO, NOT SINCE 1938!"
     end
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
 end
 
 speak_to_grandma("HELLO THERE")
