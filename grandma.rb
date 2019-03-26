2a89d2bdc0fc7d6a0aa0f266b365243521d2fc33
Angela Mumba <malaika.mumba@gmail.com>
2019-03-26 20:08:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -23,5 +23,8 @@ def speak_to_grandma(speak)
     
   elsif speak == speak.downcase
       return "HUH?! SPEAK UP, SONNY!"
+      
+    elsif speak == speak.upcase 
+    return "NO,NOT SINCE 1938"
   end
 end
